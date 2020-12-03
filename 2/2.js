d132cb9bac06d88fe098f2ddce1a0fb5fc6f46ce
HoraJan <34576942+HoraJan@users.noreply.github.com>
2020-12-03 08:31:49 +0100

    refactor

--- 2/2.js
+++ 2/2.js
@@ -1,21 +1,25 @@
+// 2-9 c: ccccccccc
+function regexpMatch(str) {
+  return str.match(
+    /\s*(?<first>\d+)-(?<second>\d+)\s(?<letter>\w):\s(?<haystick>\w+)/
+  );
+}
+
 function solution2first(arr) {
   const lines = arr.split("\n");
   const filteredLines = lines.filter((line) => {
-    const [times, letter, haystick] = line.replace(":", "").trim().split(" ");
-    const [low, high] = times.split("-");
-    const numberOfLetters = haystick
-      .split("")
-      .filter((piece) => letter === piece).length;
+    const { first, second, letter, haystick } = regexpMatch(line).groups;
+    const letters = haystick.match(new RegExp(letter, "g"));
+    const numberOfLetters = letters ? letters.length : 0;
 
-    return low <= numberOfLetters && numberOfLetters <= high;
+    return first <= numberOfLetters && numberOfLetters <= second;
   });
   return filteredLines.length;
 }
 function solution2second(arr) {
   const lines = arr.split("\n");
   const filteredLines = lines.filter((line) => {
-    const [times, letter, haystick] = line.replace(":", "").trim().split(" ");
-    const [first, second] = times.split("-");
+    const { first, second, letter, haystick } = regexpMatch(line).groups;
     const splitted = haystick.split("");
     const firstCheck = splitted[parseInt(first) - 1] === letter;
     const secondCheck = splitted[parseInt(second) - 1] === letter;
